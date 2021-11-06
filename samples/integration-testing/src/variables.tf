9b2968b9def045e7f45a0ffe58c5d441117f2802
Javier Chamorro <javier.chamorro@grupobabel.com>
2021-11-05 23:19:43 -0500

    cambio de canfiguracion provider

--- samples/integration-testing/src/variables.tf
+++ samples/integration-testing/src/variables.tf
@@ -2,5 +2,4 @@ variable location {
   type        = string
   description = "The Azure location where the resources will be created"
   default     = "westeurope"
-  skip_provider_registration = true
 }
