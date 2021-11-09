ef95888b718b4ae3816ce2240519587819bc58b8
Javier Chamorro <javier.chamorro@grupobabel.com>
2021-11-08 19:40:15 -0500

    touch Checkov-Report.xml 2

--- samples/integration-testing/src/checkov.sh
+++ samples/integration-testing/src/checkov.sh
@@ -23,7 +23,7 @@ chmod 777 Checkov-Report.xml
 #######################################
 run_checkov() {
   local test_dir=$1
-  docker run -t -v "${test_dir}":/tf bridgecrew/checkov:latest -d /tf -o junitxml >> /tf/Checkov-Report.xml
+  docker run -t -v "${test_dir}":/tf bridgecrew/checkov:latest -d /tf -o junitxml >> Checkov-Report.xml
 }
 
 #######################################
